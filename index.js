c810b4a183659ba97eea3347c67dac8057fd127b
babaYogii <yogeshkodlinge@gamil.com>
2023-01-22 10:23:37 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const hamburger=document.querySelector('.header .nav-bar .nav-list .hamburger')
+const mobile_menu=document.querySelector('.header .nav-bar .nav-list ul')
+const header=document.querySelector('.header.container')
+
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+})
+
+document.addEventListener('scroll',()=>{
+    var scrollPosition=window.scrollY;
+    console.log(scrollPosition);
+    if(scrollPosition>250){
+      header.style.backgroundColor="#29323c"
+    }else{
+        header.style.backgroundColor="transparent"
+    }
+})
